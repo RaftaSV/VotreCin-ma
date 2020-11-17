19468902454a0ad6f866285afc72fd532d43e959
RaftaSV <62962896+RaftaSV@users.noreply.github.com>
2020-11-16 19:15:27 -0600

    ajuste fecha tabla pelicula

--- VotreCinema/src/main/java/com/unab/edu/sv/Formularios/fPeliculas.java
+++ VotreCinema/src/main/java/com/unab/edu/sv/Formularios/fPeliculas.java
@@ -169,6 +169,7 @@ public class fPeliculas extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
       SimpleDateFormat formato = new SimpleDateFormat("d MMM y");
+      SimpleDateFormat formatohora = new SimpleDateFormat("HH:MM");
 
     public void ajustartabla() {
         // Ocultar la columna del ID de la pelicula
@@ -237,7 +238,7 @@ public class fPeliculas extends javax.swing.JInternalFrame {
             // a justicifado
             fila[2] = "<HTML>" + "<p align=\"justify\">" + i.getNombre() + "</HTML>";
             fila[3] = "<HTML>" + "<p align=\"justify\">" + String.valueOf(formato.format(i.getYear())) + "</HTML>";
-            fila[4] = "<HTML>" + "<p align=\"justify\">" + i.getDuracion() + "</HTML>";
+            fila[4] = "<HTML>" + "<p align=\"justify\">" + String.valueOf(formatohora.format(i.getDuracion())) + "</HTML>";
             if (i.getClasificacion() == 0) {
                 fila[5] = "<HTML>" + "<p align=\"justify\">" + "Para toda la familia" + "</HTML>";
             } else if (i.getClasificacion() == 1) {
